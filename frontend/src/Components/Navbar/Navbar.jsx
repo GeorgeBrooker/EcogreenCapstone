cfc6fe0e7c3e1b1faa350c59227d517e7c1c7518
kann-na-zu-ki <gaoshihan.e@gmail.com>
2024-05-04 20:14:20 +1200

    add some commented code for connection in future

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -24,7 +24,9 @@ const Navbar = () => {
                 <li onClick={()=>{setMenu("contactUs")}}><Link style={{textDecoration: 'none'}} to='/contactUs'>Contact Us</Link>{menu==="contactUs"?<hr />:<></> }</li>
             </ul>
             <div className="nav-login-cart">
-                <Link to='/login'><button>Login</button></Link>
+                {localStorage.getItem('auth-token')?
+                <button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/home')}}>Logout</button>
+                :<Link to='/login'><button>Login</button></Link>}
                 <Link to='/cart'><img src={cart_icon} alt='' /></Link>
                 <div className="nav-cart-count">{getTotalCartItems()}</div>
             </div>
