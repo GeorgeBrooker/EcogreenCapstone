86ca16acb0a6937ef638d256cff5e4574be0cd95
Miazhu123123 <yaominzhu7@gmail.com>
2024-05-10 21:45:06 +1200

    a part of the admin page

---
+++ admin-page/vite-project/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Navbar.css';
+import navlog from '../../assets/logo.png'
+
+
+const Navbar = () =>{
+  return(
+    <div className="navbar">
+        <img src={navlog} alt="" className="nav-logo"></img>
+         
+      
+
+
+    </div>
+  )
+}
+export default Navbar;
