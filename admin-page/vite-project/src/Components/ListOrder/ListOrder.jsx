a2a54025998e415e26c217ceed5fb1e3bef29400
Miazhu123123 <yaominzhu7@gmail.com>
2024-06-01 15:10:49 +1200

    nzpost in admin page

--- admin-page/vite-project/src/Components/ListOrder/ListOrder.jsx
+++ admin-page/vite-project/src/Components/ListOrder/ListOrder.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import './ListOrder.css';
 import {serverUri} from "../../App.jsx";
+import Modal from "../Modal/Modal";
+import Nzpostdetail from '../Nzpostdetail/Nzpostdetail'
 
 const ListOrder = () => {
     const [allCustomers, setCustomers] = useState([]);
+    const [selectedOrder, setSelectedOrder] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
 
     const fetchInfo = async () => {
         const response = await fetch(`${serverUri}/api/shop/GetOrders`);
@@ -14,6 +19,14 @@ const ListOrder = () => {
     useEffect(() => {
         fetchInfo();
     }, []);
+    const openModal = (order) => {
+        setSelectedOrder(order);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
 
     return (
         <div className="listorder">
@@ -26,6 +39,7 @@ const ListOrder = () => {
                 <div className="column deliveryLabelUid">deliveryLabelUid</div>
                 <div className="column time">time</div>
                 <div className="column amount">amount</div>
+                <div className="parcelinformation">parcel</div>
                  
             </div>
             <div className="listorder-allorder">
@@ -38,9 +52,15 @@ const ListOrder = () => {
                         <div className="column deliveryLabelUid">{Order.deliveryLabelUid}</div>
                         <div className="column time"></div>
                         <div className="column amount"></div>
+                        <button className = 'detail' onClick={() => openModal(Order)}>Detail</button>
                     </div>
                 ))}
             </div>
+            {isModalOpen && (
+                <Modal isOpen={isModalOpen} onClose={closeModal}>
+                    <Nzpostdetail order={selectedOrder} />
+                </Modal>
+                )}
         </div>
     );
 }
